920cff28b11141b4ef2f57fee8cd68ef11091998
Steven Beck <stevenrbeck@hotmail.com>
2018-06-07 07:25:32 -0400

    latest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1> An Updated Angular CI Example for Noobies! </h1>
+    <h1> An Updated Angular CI Example for Noobies and others! </h1>
     <nav>
       <a routerLink="/red">RED</a>
       <a routerLink="/blue">BLUE</a>
